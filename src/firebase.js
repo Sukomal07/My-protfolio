81809a112777243a0015c483da3e7aa498f3d41b
Sukomal07 <hypersukomal@gmail.com>
2023-04-11 11:22:38 +0530

    remove env

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
 const firebaseConfig = {
-  apiKey: "AIzaSyA2J6MSRJw06bipwbg89PC83xYTw55TAJ8",
-  authDomain: "protfolio-sukomal07.firebaseapp.com",
-  projectId: "protfolio-sukomal07",
-  storageBucket: "protfolio-sukomal07.appspot.com",
-  messagingSenderId: "416009791083",
-  appId: "1:416009791083:web:7e09302d0070a54ec7b32e"
+  apiKey: `${process.env.REACT_APP_FIREBASE_API}`,
+  authDomain: `${process.env.REACT_APP_AUTH}`,
+  projectId: `${process.env.REACT_APP_Id}`,
+  storageBucket: `${process.env.REACT_APP_Bucket}`,
+  messagingSenderId: `${process.env.REACT_APP_SenderId}`,
+  appId: `${process.env.REACT_APP_appId}`
 };
 
 // Initialize Firebase
